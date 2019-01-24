5ee40f23000eb18abdf63824d766cc82dc6fd83a
junyw <wangjunyi13@gmail.com>
2019-01-23 20:52:20 -0500

    Implement tagging and anf

--- boa/compile.ml
+++ boa/compile.ml
@@ -28,8 +28,32 @@ type tag = int
 (* PROBLEM 2 *)
 (* This function assigns a unique tag to every subexpression and let binding *)
 let tag (e : 'a expr) : tag expr =
-  failwith "tag: Implement this"
+  let rec help (e : 'a expr) (cur : tag) : (tag expr * tag) =
+    match e with
+    | ENumber(n, _) -> (ENumber(n, cur), cur + 1)
+    | EId(x, _)     -> (EId(x, cur), cur + 1)
+    | EPrim1(op, e, _)       -> let (tag_e, next_tag) = help e (cur + 1) in
+                                  (EPrim1(op, tag_e, cur), next_tag)
+    | EPrim2 (op, e1, e2, _) -> let (tag_e1, next_tag) = help e1 (cur + 1) in
+                                let (tag_e2, next_tag') = help e2 next_tag in
+                                  (EPrim2(op, tag_e1, tag_e2, cur), next_tag')
+    | ELet(binds, body, _)   -> let (tag_binds, next_tag) = 
+                                  List.fold_left 
+                                  (fun (tag_binds, cur) (x, expr, _) -> 
+                                    let (tag_expr, next_tag) = help expr (cur + 1) in
+                                        (tag_binds @ [(x, tag_expr, cur)], next_tag))
+                                  ([], cur + 1) binds
+                                in 
+                                let (tag_body, next_tag') = help body next_tag in
+                                  (ELet(tag_binds, tag_body, cur), next_tag')
+    | EIf(cond, thn, els, _) -> let (tag_cond, next_tag) = help cond (cur + 1) in
+                                let (tag_thn, next_tag') = help thn next_tag in
+                                let (tag_els, next_tag'') = help els next_tag' in
+                                  (EIf(tag_cond, tag_thn, tag_els, cur), next_tag'')
+  in
+  let (tagged, _) = help e 1 in tagged
 ;;
+
 
 (* This function removes all tags, and replaces them with the unit value.
    This might be convenient for testing, when you don't care about the tag info. *)
@@ -49,9 +73,31 @@ let rec untag (e : 'a expr) : unit expr =
 (* PROBLEM 3 & 4 *)
 (* This function converts a tagged expression into an untagged expression in A-normal form *)
 let anf (e : tag expr) : unit expr =
-  failwith "anf: Implement this"
+  (* The result is a pair of an answer and a context.
+     The answer must be an immediate, and the context must be a list of bindings
+     that are all in ANF. *)
+  let rec helper (e : tag expr) : (unit expr * (string * unit expr * unit) list) =
+    match e with
+    | EId(x, _)          -> (EId(x, ()), [])
+    | ENumber(n, _)      -> (ENumber(n, ()), [])
+    | EPrim1(op, e, tag) -> let (e_anf, e_ctxt) = helper e in
+                            let temp = sprintf "$eprim1_%d" tag in
+                              (EId(temp, ()), 
+                               e_ctxt @ (* the context needed for the expression *)
+                               [(temp, EPrim1(op, e_anf, ()), ())]) (* definition of the answer *)
+    | EPrim2(op, e1, e2, tag) ->  let (e1_anf, e1_ctxt) = helper e1 in
+                                  let (e2_anf, e2_ctxt) = helper e2 in
+                                  let temp = sprintf "$eprim2_%d" tag in
+                                  (EId(temp, ()), 
+                                   e1_ctxt @ (* the context needed for the left expression *)
+                                   e2_ctxt @ (* the context needed for the right expression *)
+                                   [(temp, EPrim2(op, e1_anf, e2_anf, ()), ())]) (* definition of the answer *)
+    | ELet(binds, body, _)   -> failwith "anf let not implemented"   
+    | EIf(cond, thn, els, _) -> failwith "anf if not implemented"
+  in
+  let (e_tag, bindings_tag) = helper e in
+    ELet(bindings_tag, e_tag, ())
 ;;
-
 
 (* Helper functions *)
 let r_to_asm (r : reg) : string =
@@ -99,17 +145,17 @@ let rec compile_expr (e : tag expr) (si : int) (env : (string * int) list) : ins
   | EPrim1(op, e, _) ->
      let e_reg = compile_imm e env in
      begin match op with
-     | Add1 -> [
-         IMov(Reg(EAX), e_reg);
-         IAdd(Reg(EAX), Const(1))
-       ]
-     | Sub1 -> [
-         IMov(Reg(EAX), e_reg);
-         IAdd(Reg(EAX), Const(~-1))
-       ]
+     | Add1 -> [ IMov(Reg(EAX), e_reg); IAdd(Reg(EAX), Const(1))   ]
+     | Sub1 -> [ IMov(Reg(EAX), e_reg); IAdd(Reg(EAX), Const(~-1)) ]
      end
-  | EPrim2(op, left, right, _) ->
-     failwith "compile_expr:eprim2: Implement this"
+  | EPrim2(op, e1, e2, _) ->
+     let e1_reg = compile_imm e1 env in
+     let e2_reg = compile_imm e2 env in
+     begin match op with
+     | Plus  -> [ IMov(Reg(EAX), e1_reg); IAdd(Reg(EAX), e2_reg) ]
+     | Minus -> [ IMov(Reg(EAX), e1_reg); ISub(Reg(EAX), e2_reg) ]
+     | Times -> [ IMov(Reg(EAX), e1_reg); IMul(Reg(EAX), e2_reg) ]
+     end
   | EIf(cond, thn, els, tag) ->
      failwith "compile_expr:eif: Implement this"
   | ELet([id, e, _], body, _) ->
